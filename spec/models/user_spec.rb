6985811d57eb275fc99acbf6b372221bdad523ae
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-13 10:52:10 -0800

    test: wrote test for user password/digest/authentication in model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe User do
@@ -12,11 +10,15 @@ RSpec.describe User do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password_digest }
+    it { should have_secure_password }
   end
 
   it 'can get the user name with email' do
-    user = create(:user)
+    user = create(:user, password: "test_password")
 
     expect(user.name_with_email).to eq("#{user.name} #{user.email}")
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq("test_password")
   end
 end
